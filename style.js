37efa4ef29e99fe4259aa4156e199d4e9b2c495e
Dima <ldmenergo@yandex.ru>
2023-03-13 22:29:34 +0500

    1st_task_done

--- style.js
+++ style.js
@@ -12,13 +12,29 @@ function insertData () {
         return inputArrs;
      };
 
+ function findIndex (myArray, numberElemarray, indexArray) {
+                    let index = myArray.indexOf(myArray[numberElemarray],indexArray);
+                        while (index > 0) {
+                            myArray.splice (index,1);
+                            index = myArray.indexOf(myArray[numberElemarray],indexArray);                        
+                        };
+                };
+
 let cod = prompt
 ('Введите код задания\n 1-На вход поступает массив, вывести массив, удалив неуникальные значения\n 2-На вход поступает массив, реверсировать значения (подобно методу reverse) метод reverse не использовать\n Esc-Выход');
 switch (cod) {
-    case '1' :  let newArrs =insertData(); 
+    case '1' :  
+                let newArrs = insertData(); 
+                let elem2 = 0;
+                              
+                for (let elem = 0; elem < newArrs.length; elem++) {
+                    findIndex (newArrs, elem, ++elem2);     
+                }
+                console.log('Уникальный массив выглядит так   ' + newArrs);
                 
     break;
-    case '2' : 
+    case '2' :   let newArrs1 = insertData(); 
+
                 // let elem2 = prompt ('Введите элемент массива');
                 // let newArrs2 =[];
                 // let newArrs2Revers =[];
